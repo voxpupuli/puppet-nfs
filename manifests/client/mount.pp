e6845c55ae893786db68a6f85fd66310ba4f9b54
derdanne <dk@marketing-factory.de>
2014-12-19 11:38:45 +0100

    added documentation and example to nfs::client::mount

--- manifests/client/mount.pp
+++ manifests/client/mount.pp
@@ -5,17 +5,51 @@
 #
 # === Parameters
 #
-# TODO: has to be filled
+# [*server*]
+#   String. Sets the ip address of the server with the nfs export
+#
+# [*share*]
+#   String. Sets the name of the nfs share on the server
+#
+# [*ensure*]
+#   String. Sets the ensure parameter of the mount.
+#
+# [*remounts*]
+#   String. Sets the remounts parameter of the mount.
+#
+# [*atboot*]
+#   String. Sets the atboot parameter of the mount.
+#
+# [*options_nfsv4*]
+#   String. Sets the mount options for a nfs version 4 mount.
+#
+# [*options_nfs*]
+#   String. Sets the mount options for a nfs mount.
+#
+# [*bindmount*]
+#   String. When not undef it will create a bindmount on the node
+#   for the nfs mount.
+#
+# [*nfstag*]
+#   String. Sets the nfstag parameter of the mount.
+#
+# [*nfs_v4*]
+#   Boolean. When set to true, it uses nfs version 4 to mount a share.
 #
 # === Examples
 #
-# TODO: has to be filled
+# class { '::nfs':
+#   client_enabled => true,
+#   nfs_v4_client => true
+# }
 #
-#
-# === Links
-#
-# * {Puppet Docs: Using Parameterized Classes}[http://j.mp/nVpyWY]
-#
+# nfs::client::mount { '/target/directory':
+#   server => '1.2.3.4',
+#   share => 'share_name_on_nfs_server',
+#   remounts => true,
+#   atboot => true,
+#   options_nfsv4 => 'tcp,nolock,rsize=32768,wsize=32768,intr,noatime,actimeo=3'
+#   }
 #
 # === Authors
 #
