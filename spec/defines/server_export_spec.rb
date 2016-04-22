5707837cd5bf79fadf79892749d56d46a1c4c99b
Tim Meusel <tim@bastelfreak.de>
2016-04-22 17:57:46 +0200

    enable Style/BracesAroundHashParameters

--- spec/defines/server_export_spec.rb
+++ spec/defines/server_export_spec.rb
@@ -18,7 +18,7 @@ describe 'nfs::server::export', type: 'define' do
 
     let(:params) { { clients: '1.2.3.4(rw)' } }
     it do
-      should contain_nfs__functions__create_export('/srv/test').with({ 'ensure' => 'mounted', 'clients' => '1.2.3.4(rw)', })
+      should contain_nfs__functions__create_export('/srv/test').with('ensure' => 'mounted', 'clients' => '1.2.3.4(rw)')
     end
   end
   context 'nvs_v4 => true' do
@@ -36,10 +36,10 @@ describe 'nfs::server::export', type: 'define' do
 
     let(:params) { { clients: '1.2.3.4(rw)', bind: 'sbind' } }
     it do
-      should contain_nfs__functions__nfsv4_bindmount('/srv/test').with({ 'ensure' => 'mounted', 'v4_export_name' => 'test', 'bind' => 'sbind', })
+      should contain_nfs__functions__nfsv4_bindmount('/srv/test').with('ensure' => 'mounted', 'v4_export_name' => 'test', 'bind' => 'sbind')
     end
     it do
-      should contain_nfs__functions__create_export('/export/test').with({ 'ensure' => 'mounted', 'clients' => '1.2.3.4(rw)', })
+      should contain_nfs__functions__create_export('/export/test').with('ensure' => 'mounted', 'clients' => '1.2.3.4(rw)')
     end
   end
 end
