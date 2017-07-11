fe649514464bdcf46cb03d4286e010aac706400a
Daniel Klockenkämper <dk@marketing-factory.de>
2017-07-11 16:19:02 +0200

    remove rpc.idmapd from $client_nfsv4_services for Archlinux rename rpc.idmapd to nfs-idmapd in $server_nfsv4_servicehelper for archlinux

--- manifests/params.pp
+++ manifests/params.pp
@@ -187,8 +187,8 @@ class nfs::params {
       $client_services            = {'rpcbind' => {}}
       $client_nfsv4_fstype        = 'nfs4'
       $client_nfsv4_options       = 'tcp,nolock,rsize=32768,wsize=32768,intr,noatime,nfsvers=4,actimeo=3'
-      $client_nfsv4_services      = {'rpcbind' => {}, 'rpc.idmapd' => {}}
-      $server_nfsv4_servicehelper = 'rpc.idmapd'
+      $client_nfsv4_services      = {'rpcbind' => {}}
+      $server_nfsv4_servicehelper = 'nfs-idmapd'
       $server_service_name        = 'nfs-server.service'
     }
     default: {
