88babe4533c956e1c180d99d746294a7d72d975a
Svante Bengtson <svante@swantzter.se>
2023-09-22 17:44:08 +0000

    Enable support for debain 12 bookworm

--- manifests/params.pp
+++ manifests/params.pp
@@ -134,7 +134,7 @@ class nfs::params {
           $server_service_name        = 'nfs-kernel-server'
           $client_gssdopt_name        = 'RPCGSSDOPTS'
         }
-        'bullseye', 'buster', 'stretch', 'xenial', 'yakkety', 'zesty': {
+        'bookworm', 'bullseye', 'buster', 'stretch', 'xenial', 'yakkety', 'zesty': {
           $client_services            = { 'rpcbind' => {
                                             ensure => 'running',
                                             enable => false,
